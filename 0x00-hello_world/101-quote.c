b4c5e0054c67e85f30a4dc76237d8202cf16eb5f
Olumide, S. A <simiolumide@gmail.com>
2022-04-07 20:42:09 -0700

    editted

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char*quote = "and that piece of art is useful\" - Dora Korpar, 2005-10-19;
+        write(2,quote, 59);
+	return (1);
+}
