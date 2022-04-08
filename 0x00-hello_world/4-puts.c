83e9acc7c0d4efa88214d0167436927fdb8a7c0d
Olumide, S. A <simiolumide@gmail.com>
2022-04-07 18:40:42 -0700

    ne changes made now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+
 /**
- * This is a simple C progam that prints a string of words.
- * This is my first C program to be executed. Woahhhhh!!!!
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
