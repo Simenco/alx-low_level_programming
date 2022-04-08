8177f2efed3413fc190e860c5b79912b704500d8
Olumide, S. A <simiolumide@gmail.com>
2022-04-07 18:37:21 -0700

    ne changes made now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * This is a simple C progam that prints a string of words.
  * This is my first C program to be executed. Woahhhhh!!!!
  * main - Entry point
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
