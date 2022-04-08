8b1110fa48fb80a83721a997122ee7a761aa344f
Olumide, S. A <simiolumide@gmail.com>
2022-04-07 20:51:16 -0700

    editted

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-        char*quote = "and that piece of art is useful\" - Dora Korpar, 2005-10-19;
-        write(2,quote, 59);
-	return (1);
+  char*quote = "and that piece of art is useful\" - Dora Korpar, 2005-10-19;
+  write(2,quote, 59);
+return (1);
 }
