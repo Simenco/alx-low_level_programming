7cdb415103302a939c7192f09fedce516dfdbc31
Olumide, S. A <simiolumide@gmail.com>
2022-04-07 18:16:50 -0700

    added new files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * This is a simple C progam that prints a string of words.
+ * This is my first C program to be executed. Woahhhhh!!!!
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
