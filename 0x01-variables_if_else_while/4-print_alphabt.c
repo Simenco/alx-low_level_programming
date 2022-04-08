f42f2c2a5f52f38b3804dff3a3c8bb2579c8fa39
Olumide, S. A <simiolumide@gmail.com>
2022-04-08 16:30:19 -0700

    this program prints a-z except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'e' || alph == 'q')
+		{
+		}
+		else
+		{
+			putchar(alph);
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
