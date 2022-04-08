a730af6bd94710f0b310d75433ffc5a301852db6
Olumide, S. A <simiolumide@gmail.com>
2022-04-07 20:15:53 -0700

    editted

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,16 @@
  */
 int main(void)
 {
-	clrscr();
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	getch();
-}
+	char type_Char;
+	int type_int;
+	long type_Lint;
+	long long type_LLint;
+	float type_float;
+	printf("Size of a char: %lu byte(s)\n", sizeof(type_Char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(type_int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(type_Lint));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(type_LLint));
+	printf("Size of a float: %lu byte(s)\n", sizeof(type_float));
+
 	return (0);
+}
