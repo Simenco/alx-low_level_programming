aeb452e0dd895a29c25783c24a0045a6b35646e0
Olumide, S. A <simiolumide@gmail.com>
2022-04-14 01:48:38 -0700

    function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,10 +19,28 @@ void times_table(void)
 
 			k = i * j;
 
-			_putchar(k / 10 + '0');
-			_putchar(k % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			if ((k / 10) == 0) && ((k % 10) == 0)
+			{
+				_putchar(' ');
+				_putchar('0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((k / 10) == 0) && ((k % 10) != 0)
+                        {
+                                _putchar(' ');
+                                _putchar((k % 10) + '0');
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+			else
+                        {
+                                _putchar((k / 10) + '0');
+                                _putchar((k % 10) + '0');
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+ 
 		}
 		_putchar('\n');
 	}
