346841e5d389de2fe4db805674da6fbf890d7d11
Olumide, S.A <simiolumide@gmail.com>
2022-04-21 00:15:11 +0000

    lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns length of string
+ * @s: the string input
+ * Return: int value, the length of str
+ */
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len += i;
+	}
+	return (len);
+}
