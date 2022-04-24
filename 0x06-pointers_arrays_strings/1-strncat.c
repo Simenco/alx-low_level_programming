53ca7119f6ca0a19561165fe4e7ce9c1245f3ac3
Olumide, S.A <simiolumide@gmail.com>
2022-04-24 14:22:17 +0000

    cat two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,29 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j < n; j++)
+	for (k = 0; src[k] != '\0'; k++)
 	{
-		dest[i] = src[j];
-		i++;
+	}
+	if (n <= k)
+	{
+		for (j = 0; j < n; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+	}
+	else
+	{
+		for (j = 0; j < k; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
 	}
 	dest[i] = '\0';
 	return (dest);
