1b89eeef3e7f7d165b59ed1fa212b176338e62f6
Olumide, S.A <simiolumide@gmail.com>
2022-04-24 17:14:23 +0000

    compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: first string pointer
+ * @s2: second string pointer
+ * Return: int value. +, 0 or - values are returned
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int diff;
+
+	diff = s1[i] - s2[j];
+
+
+	return (diff);
+}
