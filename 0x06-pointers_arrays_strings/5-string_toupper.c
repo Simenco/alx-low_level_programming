33982ea6df44209ca95eb4fc118e98757a1bd3b2
Olumide, S.A <simiolumide@gmail.com>
2022-04-24 19:03:49 +0000

    function to change lower case string to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lower cases to upper
+ *
+ * Return: char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 97) && (str[i] <= 122))
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
