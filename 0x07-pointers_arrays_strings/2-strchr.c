24608cc2d8795e9aabe22dea3f57aeaeef4e6445
Olumide, S.A <simiolumide@gmail.com>
2022-04-26 20:58:31 -0700

    finds the location of a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to search for
+ * Return: pointer to the first occurrence
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int n;
+	
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		if (c == s[n])
+		{
+			break;
+		}
+	}
+	return(c == s[n] ? (s + n) : '\0');
+}
