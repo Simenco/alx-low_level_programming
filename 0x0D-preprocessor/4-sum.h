2dac095ed772db20caa385319a973be28c332b73
Olumide, S.A <simiolumide@gmail.com>
2022-05-11 13:57:03 -0700

    this macro computes the sum of x & y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * file - 4-sum.h
+ * @x: parameter
+ * @y: parameter
+ * this macro SUM(x, y) computes the sum of x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
