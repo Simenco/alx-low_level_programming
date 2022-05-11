2df310add18fbb79d7d93adcc11eac8b6170e9e6
Olumide, S.A <simiolumide@gmail.com>
2022-05-11 10:37:44 -0700

    this program prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * desc: this prints the name of the file
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("File : %s\n", __FILE__);
+
+	return (0);
+}
