c458b229e9ba7e9b9babb8509a14bd154b19585f
Olumide, S.A <simiolumide@gmail.com>
2022-05-11 10:57:27 -0700

    function like macro that computes absolute value of number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS_H
+#define ABS_H
+
+/**
+ * file: 3-function_like_macro.h
+ *
+ * this macro ABS(X) computes the absolute value of X
+ */
+
+#define ABS(X) (X < 0 ? (-1 * X) : (X))
+
+#endif
