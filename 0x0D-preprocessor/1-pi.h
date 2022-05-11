c6be54a533bbf236fb492430614efb0299e974e1
Olumide, S.A <simiolumide@gmail.com>
2022-05-11 10:04:49 -0700

    header file for PI

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,3 +1,12 @@
-#ifndef PI
+#ifndef PI_H
+#define PI_H
+
+/**
+ * filename: 1-pi.h
+ *
+ * This macro defines PI as an abbreviation for
+ * the token 3.14159265359
+ */
+
 #define PI (3.14159265359)
 #endif
