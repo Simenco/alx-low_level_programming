6b41ed4588415502ffdcb3c9141612ecbc507c4d
Olumide, S. A <simiolumide@gmail.com>
2022-05-13 18:52:56 +0000

    structure dog.h definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef "DOG_H"
+#define "DOG_H"
+
+/**
+ * struct dog - dog structure definition
+ * @name: object1
+ * @age: object2
+ * @owner: object3
+ *
+ * desc: this structure defines dog with name, age and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
