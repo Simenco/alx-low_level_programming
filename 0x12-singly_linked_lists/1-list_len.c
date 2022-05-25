991e3dea3ad3c38aeb586e4d0aa71e9f713bfc99
Olumide, S.A <simiolumide@gmail.com>
2022-05-24 21:47:29 -0700

    function that returns num of elements in a list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function 
+ * list_len - function
  * @h: input list
  *
  * Return: number of elements in a list
@@ -13,7 +13,7 @@ size_t list_len(const list_t *h)
 	for (num_elements = 0; h; num_elements++)
 	{
 		num_elements++;
-		h = h->next; 
+		h = h->next;
 	}
 	return (num_elements);
 
