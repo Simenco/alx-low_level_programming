7b8d20fc05b510fb140f804ac223b3c4eda98320
Olumide, S.A <simiolumide@gmail.com>
2022-05-26 16:25:13 -0700

    prints number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - the function returns the number of elements
+ * 
+ * @h: head pointer
+ *
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	/*const listint_t *movptr = NULL;*/
+	int num_of_nodes = 0;
+
+	/*if (h == NULL)
+		return (num_of_nodes);
+	movptr = h;*/
+	while (h != NULL)
+	{
+		num_of_nodes++;
+		/*printf("%d\n", h->n);*/
+		h = h->next;
+	}
+	return (num_of_nodes);
+
+}
