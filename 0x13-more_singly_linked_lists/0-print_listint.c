707757ba36c1b5edad0099fa0317cbc7e7e937de
Olumide, S.A <simiolumide@gmail.com>
2022-05-26 16:05:23 -0700

    the function prints the elements in the nodes and return the number of nodes present.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	listint_t *movptr = NULL;
+	const listint_t *movptr = NULL;
 	int num_of_nodes = 0;
 
 	if (h == NULL)
@@ -19,7 +19,7 @@ size_t print_listint(const listint_t *h)
 	while (movptr != NULL)
 	{
 		num_of_nodes++;
-		printf("%d", movptr->n);
+		printf("%d\n", movptr->n);
 		movptr = movptr->next;
 	}
 	return (num_of_nodes);
