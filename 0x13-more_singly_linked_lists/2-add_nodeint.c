7f5e5278fc04406d019fbca5db914d163651f102
Olumide, S.A <simiolumide@gmail.com>
2022-05-26 17:15:15 -0700

    adds new nodes and prints its address

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * add_nodeint - the function adds nodes at the beginning
+ *
+ * @head: head pointer
+ * @n: data of the node to be added
+ *
+ * Return: address of the new element
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *movptr = malloc(sizeof(listint_t));
+
+	if (movptr == NULL)
+		return (NULL);
+
+	movptr->n = n;
+	movptr->next = *head;
+	*head = movptr;
+	
+	return (movptr);
+
+}
