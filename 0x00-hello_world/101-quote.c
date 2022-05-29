33fe439c37faf7589cd01e5fa68a62b0c08d0fd3
Olumide, S.A <simiolumide@gmail.com>
2022-05-28 20:27:28 -0700

    doing revision and completing some assignements I did not complete initially.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  char*quote="and that piece of art is useful\" - Dora Korpar, 2005-10-19;
-  write(2,quote, 59);
-return (1);
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2005-10-19\n";
+	write(2, quote, 59);
+	return (1);
 }
