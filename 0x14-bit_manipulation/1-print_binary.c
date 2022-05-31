64996bc97e2eaf544cd4f6c52d61931cd6c76c55
Olumide, S.A <simiolumide@gmail.com>
2022-05-31 01:35:56 -0700

    funnction that converts number to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,17 +8,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int j, binary[INT_SIZE];
-	int i = INT_SIZE - 1;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (i >= 0)
-	{
-		binary[i] = n & 1;
-		i--;
-		n = n >> 1;
-	}
-	for (j = 0; j < INT_SIZE; j++)
-	{
-		_putchar(binary[j] + '0');
-	}
+	_putchar((n & 1) + '0');
 }
