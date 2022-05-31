a050b8b3fcf59a9e066068a265723d6c2f709903
Olumide, S.A <simiolumide@gmail.com>
2022-05-31 14:50:08 -0700

    clear a bit at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - set the bit value at any index to 0
+ * @n: the number
+ * @index: index where value will be printed
+ * Return: value at index or -1 if error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int a;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	a = 1 << index;
+
+	*n = *n & ~a;
+	return (1);
+}
