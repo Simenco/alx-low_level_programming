1316cf240e1bb6b409524a5e4cdc5e9913d98803
Olumide, S.A <simiolumide@gmail.com>
2022-06-23 03:23:38 -0700

    added some functions to the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNC_H
 #define VARIADIC_FUNC_H
 
+typedef struct printer
+{
+        char *input;
+        void (*print_type)(va_list ap);
+} print_t;
+
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
