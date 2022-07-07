b5bf5aab259db21349426f2254c6b772e46834e0
Olumide, S.A <simiolumide@gmail.com>
2022-07-07 06:13:31 -0700

    to print elements of DLL

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - the function prints
+ *
+ * @h: the input list
+ * desc: prints all elements of list_t are printed
+ * Return: number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_nodes;
+
+	for (num_nodes = 0; h; num_nodes++)
+	{
+		if (h == NULL)
+			return (num_nodes);
+		else
+			printf("%d\n", h->n);
+		h = h->next;
+
+	}
+	return (num_nodes);
+}
