844bd0735f6211b9d8a90d9b4d9b23ae81a00ae8
Olumide, S.A <simiolumide@gmail.com>
2022-07-07 09:54:06 -0700

    add nodes at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,21 +14,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	*head->prev = NULL;
-
-	if (*head->next == NULL)
-	{
-		new-node->n = n;
-		new_node->next = NULL;
-		new_node->prev = *head;
-		return (new_node);
-	}
-
-	new_node->n = n;
+	new-node->n = n;
+		
 	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if *head != NULL
+		*head->prev = new_node;
+	
 	*head = new_node;
-
-	new_node->prev = *head;
-
 	return (new_node);
 }
