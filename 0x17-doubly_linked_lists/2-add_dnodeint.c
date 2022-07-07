3872f55f2a707e3d002de8eef860fc59ad140c09
Olumide, S.A <simiolumide@gmail.com>
2022-07-07 07:42:04 -0700

    add new nodes to DLL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
-	new_node->next = *head->next;
+	new_node->next = *head;
 	*head = new_node;
 
 	new_node->prev = *head;
