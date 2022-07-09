9b396d6b537c8fec777281fcdec324ae617cf15c
Olumide, S.A <simiolumide@gmail.com>
2022-07-09 13:01:18 -0700

    adds new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr = *head;
-	new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -37,5 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = ptr->next;
 	ptr->next = new_node;
 	new_node->n = n;
-	return (n)
+
+	return (new_node);
 }
