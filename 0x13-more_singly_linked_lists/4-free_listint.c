64443c7e73542e368e65e227c3fd22514bf602c6
Olumide, S.A <simiolumide@gmail.com>
2022-07-09 04:53:44 -0700

    frees a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint - this function frees a linked list
+ * @head: head of the linked list
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	free(head);
+}
