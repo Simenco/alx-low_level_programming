3d78dcf8e2f173d302af950fa8c7fbe582fdc98c
Olumide, S.A <simiolumide@gmail.com>
2022-07-09 13:11:04 -0700

    adds new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (ptr == NULL)
 			return (NULL);
 	}
-	
 	new_node->next = ptr->next;
 	ptr->next = new_node;
 	new_node->n = n;
