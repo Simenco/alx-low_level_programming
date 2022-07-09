d22419bab0d17dfc9b10ec072a015bc4a77e73c2
Olumide, S.A <simiolumide@gmail.com>
2022-07-09 10:32:12 -0700

    frees a list and set the head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head != NULL)
@@ -20,5 +20,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = ptr;
 	}
-	*head = NULL;
+	head = NULL;
 }
