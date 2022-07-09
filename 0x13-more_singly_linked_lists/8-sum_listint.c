e662f1347b9a89b069c202bf976dc1e60e667d96
Olumide, S.A <simiolumide@gmail.com>
2022-07-09 12:08:21 -0700

    sum of all data in a node

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - this function returns the sum of all
+ * data in a linked list
+ * @head: head of the linked list
+ * 
+ * Return: the sum or 0 if linked list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		sum += ptr->n;
+	}
+	return (sum);
+}
