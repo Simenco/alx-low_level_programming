1dc9da08ecda1344aca0b82043f4af49f977ab08
Olumide, S.A <simiolumide@gmail.com>
2022-07-11 08:11:58 -0700

    deletes node at given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,6 +22,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
+	else if (index > listint_len(*head))
+		return (-1);
+
 	else
 	{
 		for (i = 0; i < index - 1; i++)
